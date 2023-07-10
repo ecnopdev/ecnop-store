a600b9fc85ed5b16c7c7b67ef7649e5cd5c15cea
Jose Mari Ponce <jponce@mac-laptop-vmkq6ldv7f.lan>
2023-07-11 01:09:43 +1000

    Add category component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import logo from './logo.svg';
 
 const App = () => {
-  return ( <div>Hello World</div> );
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    },
+    {
+      id: 2,
+      title: 'Shoes'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    }
+  ];
+
+  return (
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div classname="category-container">
+          <div className='background-image' />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Show Now</p>
+          </div>
+        </div>
+      ))
+      }
+    </div>
+  );
 }
 
 export default App;
